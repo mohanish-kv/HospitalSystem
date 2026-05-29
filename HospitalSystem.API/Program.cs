d749194cf86b255cd637d360704c6b9f53911c4a
mohanish-kv <mohanish.kv@acuvate.com>
2026-05-29 11:26:38 +0530

    Add project files.

---
+++ HospitalSystem.API/Program.cs
@@ -0,0 +1,21 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// TODO: Add DI registrations
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+// TODO: Add middleware 
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
